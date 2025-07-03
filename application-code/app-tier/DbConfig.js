ab555a90a04297339baed6ee40dd1bd0d37747da
saurabhbalgude <114283063+saurabhbalgude@users.noreply.github.com>
2025-07-03 12:01:53 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1.creug6o4i0bs.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-2.creug6o4i0bs.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'admin12345',
     DB_DATABASE : 'webappdb'
